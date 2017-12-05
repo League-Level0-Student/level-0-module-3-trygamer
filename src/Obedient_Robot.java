24106e7edca87c8d586cd12ccc1a28ef1038c011
zahmed2003 <league@WTS10.attlocal.net>
2017-12-04 16:56:15 -0800

    Obedient_Robot.ip

---
+++ src/Obedient_Robot.java
@@ -0,0 +1,93 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Obedient_Robot {
+	
+	Robot k = new Robot();
+	static Color color= new Color(0,0,0);
+	
+public static void main(String[] args) {
+Obedient_Robot k1= new Obedient_Robot();
+String a=JOptionPane.showInputDialog("What shape do you want? (Options:  triangle, square, or circle)");
+String c=JOptionPane.showInputDialog("What color do you want? (Options:  red, green, or yellow)");
+if(c.equals("red")) {
+	
+}
+	
+else if(c.equals("green")) {
+	
+}
+	
+else {
+	color= new Color(255,0,0);
+}
+
+if(a.equals("triangle")) {
+	k1.drawTriangle();
+}
+else if(a.equals("square")) {
+	k1.drawSquare();
+	}
+else {
+	k1.drawCircle();
+	
+}
+}
+	
+	
+
+
+
+
+void drawSquare() {
+	k.setSpeed(10);
+k.penDown();
+for(int i=0;i<3;i++ ) {
+k.move(100);	
+k.turn(90);
+
+}
+k.penUp();
+
+k.move(100);
+}
+
+void drawTriangle() {
+	k.setSpeed(10);
+	k.penDown();
+	k.move(100);
+	for(int i=0;i<2;i++ ) {
+	k.turn(120);
+	k.move(100);
+	}
+k.penUp();
+	
+	k.move(100);
+	
+}
+
+void drawCircle() {
+	k.setSpeed(10);
+	k.penDown();
+	k.turn(90);
+for(int i=0;i<360;i++ ) {
+	k.move(1);
+	k.turn(1);
+	
+}
+k.penUp();
+
+k.move(100);
+}
+
+}
+	
+	
+	
+	
+	
+
+
